4e36c856fee4f4d16df9d47cdece4d3fce390f7d
cosmolejo <almego95@gmail.com>
2018-10-01 16:17:44 -0500

    new nodoDoble

--- LDL/nodoDoble.js
+++ LDL/nodoDoble.js
@@ -4,31 +4,37 @@
 
 class nodoDoble {
 
-    constructor(){
-        this.indicador= null;
-        this.lInd=null;
-        this.lCons=null; 
+    constructor() {
+        this.indicador = null;  //indicador del tipo de casilla
+        this.lInd = null;       // liga del indicador
+        this.lCons = null;      // liga consecutiva / Ld
+        this.lIzq = null;       // liga izquierda
     }
 
-    asignaIndi( i ){
-        this.indicador=i;
-    }
-    
-    asignaLInd(l){
-        this.lInd=l;
+    asignaIndi(i) {
+        this.indicador = i;
     }
 
-    asignaLCons(l){
-        this.lCons=l;
+    asignaLInd(l) {
+        this.lInd = l;
     }
-    retornaIndi(){
+
+    asignaLCons(l) {
+        this.lCons = l;
+    }
+    asignaLIzq(l){
+        this.lIzq=l;
+    }
+    retornaIndi() {
         return this.indicador;
     }
-    retornaLInd(){
+    retornaLInd() {
         return this.lInd;
     }
-    retornaLCons(){
+    retornaLCons() {
         return this.lCons;
     }
+    retornaLizq(){
+        return this.lIzq;
+    }
 }
-
